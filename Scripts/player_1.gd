5dc09741b02b5acad260521fb548e1f8327f6234
Joseph-R-21139 <21139@jpc.school.nz>
2026-06-05 00:22:19 +1200

    Tagged indicator and colosion changes
    Added an indicator that show who is currently tagged as well as changing how the collisions work with each other

--- Scripts/player_1.gd
+++ Scripts/player_1.gd
@@ -23,3 +23,11 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+	
+func _ready() -> void:
+			if TaggingScript.player_1_in == true:
+				$indicator_sprite.visible = false
+			else:
+				$indicator_sprite.visible = true
---
+++ tagging_script.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var player_1_in = true
+var player_2_in = false
+
+
+		
