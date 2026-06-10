a0f8baaeb509dc2c094ad1000f8c857db3fab172
Joseph-R-21139 <21139@jpc.school.nz>
2026-06-11 10:40:46 +1200

    main menue background
    main menu background added as well as some new buttons

--- Scripts/main_menu_buttons.gd
+++ Scripts/main_menu_buttons.gd
@@ -17,3 +17,7 @@ func _on_play__button_pressed() -> void:
 func _on_start_match_pressed() -> void:
 	if map_selected == "forrest":
 		get_tree().change_scene_to_file("res://Scenes/play_stage_forrest.tscn")
+
+
+func _on_back_to_main_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
