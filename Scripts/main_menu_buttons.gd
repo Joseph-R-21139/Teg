ac7b5525db800c2e1a762041277e1ac1683df07a
Joseph-R-21139 <21139@jpc.school.nz>
2026-06-16 09:40:18 +1200

    Remaking get ready scene
    releting and starting to remake the get ready scene in hopes of fixing it

--- Scripts/main_menu_buttons.gd
+++ Scripts/main_menu_buttons.gd
@@ -1,5 +1,4 @@
 extends Node2D
-var map_selected = "forrest"
 
 
 func _on_exit_button_pressed() -> void:
@@ -11,13 +10,4 @@ func _on_options_button_pressed() -> void:
 
 
 func _on_play__button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/get_ready_screen.tscn")
-
-
-func _on_start_match_pressed() -> void:
-	if map_selected == "forrest":
-		get_tree().change_scene_to_file("res://Scenes/play_stage_forrest.tscn")
-
-
-func _on_back_to_main_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+	get_tree().change_scene_to_file("res://match_settings.tscn")
