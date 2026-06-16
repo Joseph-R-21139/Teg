974a37556baa8deafcc8a22575710055ec602680
Joseph-R-21139 <21139@jpc.school.nz>
2026-06-16 16:13:40 +1200

    rubish fixes

--- Scripts/main_menu_buttons.gd
+++ Scripts/main_menu_buttons.gd
@@ -10,14 +10,8 @@ func _on_options_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn")
 
 
+
+
+
 func _on_play__button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/get_ready_screen.tscn")
-
-
-func _on_start_match_pressed() -> void:
-	if map_selected == "forrest":
-		get_tree().change_scene_to_file("res://Scenes/play_stage_forrest.tscn")
-
-
-func _on_back_to_main_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
