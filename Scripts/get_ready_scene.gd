d49d2effda0774b32f7cf7446fe5a3331de3e443
Joseph-R-21139 <21139@jpc.school.nz>
2026-08-20 09:42:59 +1200

    Start work on map 2
    map 2 start

--- Scripts/get_ready_scene.gd
+++ Scripts/get_ready_scene.gd
@@ -6,4 +6,8 @@ func _on_back_to_main_menu_pressed() -> void:
 
 
 func _on_start_game_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://play_stage_forrest.tscn")
+	if Global.game_stage == "forrest":
+		get_tree().change_scene_to_file("res://play_stage_forrest.tscn")
+	
+	else:
+		get_tree().change_scene_to_file("res://Scenes/play_stage_red.tscn")
